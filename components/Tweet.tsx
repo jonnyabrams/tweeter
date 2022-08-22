51bd76b6360f45e12a05b0ccb779790d7df5fc2d
jonnyabrams <jonnyabrams@hotmail.com>
2022-08-22 18:26:50 +0200

    feat: add icons to Tweet

--- components/Tweet.tsx
+++ components/Tweet.tsx
@@ -1,4 +1,10 @@
 import TimeAgo from "react-timeago";
+import {
+  ChatAlt2Icon,
+  HeartIcon,
+  SwitchHorizontalIcon,
+  UploadIcon,
+} from "@heroicons/react/outline";
 
 import { Tweet } from "../typings";
 
@@ -7,6 +13,9 @@ interface Props {
 }
 
 const Tweet = ({ tweet }: Props) => {
+  const tweetIconStyles =
+    "flex cursor-pointer items-center space-x-3 text-gray-400";
+
   return (
     <div className="flex flex-col space-x-3 border-y p-5 border-gray-100">
       <div className="flex space-x-3">
@@ -38,6 +47,25 @@ const Tweet = ({ tweet }: Props) => {
               className="m-5 ml-0 mb-1 max-h-60 rounded-lg object-cover shadow-sm"
             />
           )}
+        </div>
+      </div>
+
+      <div className="mt-5 flex justify-between">
+        <div className={tweetIconStyles}>
+          <ChatAlt2Icon className="h-5 w-5" />
+          <p>5</p>
+        </div>
+
+        <div className={tweetIconStyles}>
+          <SwitchHorizontalIcon className="h-5 w-5" />
+        </div>
+
+        <div className={tweetIconStyles}>
+          <HeartIcon className="h-5 w-5" />
+        </div>
+
+        <div className={tweetIconStyles}>
+          <UploadIcon className="h-5 w-5" />
         </div>
       </div>
     </div>
