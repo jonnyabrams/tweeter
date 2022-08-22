5c139e317b3ff0c876c01a8ee1443175366d2a45
jonnyabrams <jonnyabrams@hotmail.com>
2022-08-22 10:13:51 +0200

    feat: disable tweet button when no input text

--- components/Tweetbox.tsx
+++ components/Tweetbox.tsx
@@ -5,8 +5,14 @@ import {
   PhotographIcon,
   SearchCircleIcon,
 } from "@heroicons/react/outline";
+import { useState } from "react";
 
 const Tweetbox = () => {
+  const [input, setInput] = useState<string>("");
+
+  const iconStyles =
+    "w-5 h-5 cursor-pointer transition-transform duration-150 ease-out hover:scale-150";
+
   return (
     <div className="flex space-x-2 p-5">
       <img
@@ -17,17 +23,28 @@ const Tweetbox = () => {
 
       <div className="flex flex-1 items-center pl-2">
         <form className="flex flex-1 flex-col">
-          <input type="text" placeholder="What's happening?" className="h-24 w-full text-xl outline-none placeholder:text-xl" />
+          <input
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            type="text"
+            placeholder="What's happening?"
+            className="h-24 w-full text-xl outline-none placeholder:text-xl"
+          />
           <div className="flex items-center">
             <div className="flex flex-1 space-x-2 text-twitter">
-              <PhotographIcon className="w-5 h-5 cursor-pointer transition-transform duration-150 ease-out hover:scale-150" />
-              <SearchCircleIcon className="h-5 w-5 cursor-pointer transition-transform duration-150 ease-out hover:scale-150" />
-              <EmojiHappyIcon className="h-5 w-5 cursor-pointer transition-transform duration-150 ease-out hover:scale-150" />
-              <CalendarIcon className="h-5 w-5 cursor-pointer transition-transform duration-150 ease-out hover:scale-150" />
-              <LocationMarkerIcon className="h-5 w-5 cursor-pointer transition-transform duration-150 ease-out hover:scale-150" />
+              <PhotographIcon className={iconStyles} />
+              <SearchCircleIcon className={iconStyles} />
+              <EmojiHappyIcon className={iconStyles} />
+              <CalendarIcon className={iconStyles} />
+              <LocationMarkerIcon className={iconStyles} />
             </div>
 
-            <button className="bg-twitter px-5 py-2 font-bold text-white rounded-full">Tweet</button>
+            <button
+              disabled={!input}
+              className="bg-twitter px-5 py-2 font-bold text-white rounded-full disabled:opacity-40"
+            >
+              Tweet
+            </button>
           </div>
         </form>
       </div>
