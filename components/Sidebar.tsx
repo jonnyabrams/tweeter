0a083c9073c05c2634b897143abbf3f3f963be7f
jonnyabrams <jonnyabrams@hotmail.com>
2022-08-22 06:11:27 +0200

    styles: install heroicons & import icons to Sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,8 +1,16 @@
+import {
+  BellIcon,
+  HashtagIcon,
+  BookmarkIcon,
+  CollectionIcon,
+  DotsCircleHorizontalIcon,
+  MailIcon,
+  UserIcon,
+  HomeIcon,
+} from "@heroicons/react/outline";
 
 const Sidebar = () => {
-  return (
-    <div>Sidebar</div>
-  )
-}
+  return <div>Sidebar</div>;
+};
 
-export default Sidebar
+export default Sidebar;
