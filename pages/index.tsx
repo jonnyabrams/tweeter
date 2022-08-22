f70d570818dbbdf1982578f71e740bbeb4ba1f16
jonnyabrams <jonnyabrams@hotmail.com>
2022-08-22 17:38:38 +0200

    feat: fetching tweets from api endpoint

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,18 @@
-import type { GetServerSideProps, NextPage } from 'next'
-import Head from 'next/head'
-import Feed from '../components/Feed'
-import Sidebar from '../components/Sidebar'
-import Widgets from '../components/Widgets'
+import type { GetServerSideProps, NextPage } from "next";
+import Head from "next/head";
 
-const Home: NextPage = () => {
+import Feed from "../components/Feed";
+import Sidebar from "../components/Sidebar";
+import Widgets from "../components/Widgets";
+import { Tweet } from "../typings";
+import { fetchTweets } from "../utils/fetchTweets";
+
+interface Props {
+  tweets: Tweet[];
+}
+
+const Home = ({ tweets }: Props) => {
+  console.log(tweets)
   return (
     <div className="mx-auto max-h-screen overflow-hidden lg:max-w-6xl">
       <Head>
@@ -12,7 +20,7 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className='grid grid-cols-9'>
+      <main className="grid grid-cols-9">
         <Sidebar />
 
         <Feed />
@@ -20,17 +28,17 @@ const Home: NextPage = () => {
         <Widgets />
       </main>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const tweets = await fetchTweets();
 
   return {
     props: {
-
-    }
-  }
-}
+      tweets,
+    },
+  };
+};
