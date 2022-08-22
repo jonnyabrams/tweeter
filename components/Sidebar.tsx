5ff8f1bca5408723a40156a9a03cfa10ceeaff5a
jonnyabrams <jonnyabrams@hotmail.com>
2022-08-22 08:14:53 +0200

    styles: increase Sidebar text size on large screens

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -13,7 +13,7 @@ import SidebarRow from "./SidebarRow";
 const Sidebar = () => {
   return (
     <div className="flex flex-col col-span-2 items-center px-4 md:items-start">
-      <img className="h-10 w-10" src={"/image/twitter-logo.png"} alt="" />
+      <img className="m-3 h-10 w-10" src={"/image/twitter-logo.png"} alt="" />
       <SidebarRow Icon={HomeIcon} title="Home" />
       <SidebarRow Icon={HashtagIcon} title="Explore" />
       <SidebarRow Icon={BellIcon} title="Notifications" />
