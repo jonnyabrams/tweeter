83d0447a61fb4ec40af9ae7518b9661baab8b37f
jonnyabrams <jonnyabrams@hotmail.com>
2022-08-22 10:06:51 +0200

    styles: hover/transform effect on Tweetbox icons

--- components/Tweetbox.tsx
+++ components/Tweetbox.tsx
@@ -20,11 +20,11 @@ const Tweetbox = () => {
           <input type="text" placeholder="What's happening?" className="h-24 w-full text-xl outline-none placeholder:text-xl" />
           <div className="flex items-center">
             <div className="flex flex-1 space-x-2 text-twitter">
-              <PhotographIcon className="w-5 h-5" />
-              <SearchCircleIcon className="h-5 w-5" />
-              <EmojiHappyIcon className="h-5 w-5" />
-              <CalendarIcon className="h-5 w-5" />
-              <LocationMarkerIcon className="h-5 w-5" />
+              <PhotographIcon className="w-5 h-5 cursor-pointer transition-transform duration-150 ease-out hover:scale-150" />
+              <SearchCircleIcon className="h-5 w-5 cursor-pointer transition-transform duration-150 ease-out hover:scale-150" />
+              <EmojiHappyIcon className="h-5 w-5 cursor-pointer transition-transform duration-150 ease-out hover:scale-150" />
+              <CalendarIcon className="h-5 w-5 cursor-pointer transition-transform duration-150 ease-out hover:scale-150" />
+              <LocationMarkerIcon className="h-5 w-5 cursor-pointer transition-transform duration-150 ease-out hover:scale-150" />
             </div>
 
             <button className="bg-twitter px-5 py-2 font-bold text-white rounded-full">Tweet</button>
