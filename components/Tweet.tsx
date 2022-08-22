0058546276a1485662b8e1048e645f1f6ab1dae3
jonnyabrams <jonnyabrams@hotmail.com>
2022-08-22 18:18:05 +0200

    feat: design individual tweet component

--- components/Tweet.tsx
+++ components/Tweet.tsx
@@ -1,3 +1,5 @@
+import TimeAgo from "react-timeago";
+
 import { Tweet } from "../typings";
 
 interface Props {
@@ -5,7 +7,41 @@ interface Props {
 }
 
 const Tweet = ({ tweet }: Props) => {
-  return <div>Tweet</div>;
+  return (
+    <div className="flex flex-col space-x-3 border-y p-5 border-gray-100">
+      <div className="flex space-x-3">
+        <img
+          className="h-10 w-10 rounded-full object-cover"
+          src={tweet.profileImg}
+          alt=""
+        />
+
+        <div>
+          <div className="flex items-center space-x-1">
+            <p className="mr-1 font-bold">{tweet.username}</p>
+            <p className="hidden text-sm text-gray-500 sm:inline">
+              @{tweet.username.replace(/\s+/g, "").toLocaleLowerCase()} ·
+            </p>
+
+            <TimeAgo
+              className="text-sm text-gray-500"
+              date={tweet._createdAt}
+            />
+          </div>
+
+          <p className="pt-1">{tweet.text}</p>
+
+          {tweet.image && (
+            <img
+              src={tweet.image}
+              alt=""
+              className="m-5 ml-0 mb-1 max-h-60 rounded-lg object-cover shadow-sm"
+            />
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Tweet;
