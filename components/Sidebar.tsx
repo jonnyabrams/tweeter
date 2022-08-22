826f2842d7e75592a73fc0aa38bac58d02429d26
jonnyabrams <jonnyabrams@hotmail.com>
2022-08-22 06:35:45 +0200

    feat: create SidebarRow for each Sidebar icon

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -8,11 +8,18 @@ import {
   UserIcon,
   HomeIcon,
 } from "@heroicons/react/outline";
+import SidebarRow from "./SidebarRow";
 
 const Sidebar = () => {
   return (
     <div>
       <img className="h-10 w-10" src={"/image/twitter-logo.png"} alt="" />
+      <SidebarRow Icon={HomeIcon} title="Home" />
+      <SidebarRow Icon={HashtagIcon} title="Explore" />
+      <SidebarRow Icon={BellIcon} title="Notifications" />
+      <SidebarRow Icon={MailIcon} title="Messages" />
+      <SidebarRow Icon={BookmarkIcon} title="Bookmarks" />
+      <SidebarRow Icon={CollectionIcon} title="Lists" />
     </div>
   );
 };
