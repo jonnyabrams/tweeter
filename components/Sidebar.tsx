f5a926b7e74a16b563f3a675be86e4ac87e7c533
jonnyabrams <jonnyabrams@hotmail.com>
2022-08-22 06:24:34 +0200

    styles: bring in logo

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -10,7 +10,11 @@ import {
 } from "@heroicons/react/outline";
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <div>
+      <img className="h-10 w-10" src={"/image/twitter-logo.png"} alt="" />
+    </div>
+  );
 };
 
 export default Sidebar;
