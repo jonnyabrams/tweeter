c7e338c01197ff3c1156ce5f78a821671882caa1
jonnyabrams <jonnyabrams@hotmail.com>
2022-08-23 09:49:58 +0200

    feat: react-hot-toast refresh notifications

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,6 +1,9 @@
 import { RefreshIcon } from "@heroicons/react/outline";
+import { useState } from "react";
+import toast from "react-hot-toast";
 
 import { Tweet } from "../typings";
+import { fetchTweets } from "../utils/fetchTweets";
 import TweetComponent from "./Tweet"; // to avoid name clash
 import Tweetbox from "./Tweetbox";
 
@@ -8,12 +11,29 @@ interface Props {
   tweets: Tweet[];
 }
 
-const Feed = ({ tweets }: Props) => {
+const Feed = ({ tweets: tweetsProp }: Props) => {
+  // allows us to refresh the tweets (renaming tweets prop as tweetsProp)
+  const [tweets, setTweets] = useState<Tweet[]>(tweetsProp);
+
+  const handleRefresh = async () => {
+    const refreshToast = toast.loading("Refreshing...");
+
+    const tweets = await fetchTweets();
+    setTweets(tweets);
+
+    toast.success('Feed updated', {
+      id: refreshToast
+    }) // passing id makes it replace first toast
+  };
+
   return (
     <div className="col-span-7 lg:col-span-5 border-x">
       <div className="flex items-center justify-between">
         <h1 className="p-5 pb-0 text-xl font-bold">Home</h1>
-        <RefreshIcon className="mr-5 mt-5 h-8 w-8 cursor-pointer text-twitter transition-all duration-500 ease-out hover:rotate-180 active:scale-125" />
+        <RefreshIcon
+          onClick={handleRefresh}
+          className="mr-5 mt-5 h-8 w-8 cursor-pointer text-twitter transition-all duration-500 ease-out hover:rotate-180 active:scale-125"
+        />
       </div>
 
       <div>
@@ -21,7 +41,7 @@ const Feed = ({ tweets }: Props) => {
       </div>
 
       <div>
-        {tweets.map(tweet => (
+        {tweets.map((tweet) => (
           <TweetComponent key={tweet._id} tweet={tweet} />
         ))}
       </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { GetServerSideProps, NextPage } from "next";
 import Head from "next/head";
+import { Toaster } from "react-hot-toast";
 
 import Feed from "../components/Feed";
 import Sidebar from "../components/Sidebar";
@@ -18,6 +19,7 @@ const Home = ({ tweets }: Props) => {
         <title>Tweeter</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Toaster />
 
       <main className="grid grid-cols-9">
         <Sidebar />
