6a88f87dd72b07395c15b963fe17e8718bd612ee
jonnyabrams <jonnyabrams@hotmail.com>
2022-08-23 13:55:03 +0200

    feat: can attach image to tweet

--- components/Tweetbox.tsx
+++ components/Tweetbox.tsx
@@ -5,8 +5,7 @@ import {
   PhotographIcon,
   SearchCircleIcon,
 } from "@heroicons/react/outline";
-import { useSession } from "next-auth/react";
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 interface Props {
   session: any;
@@ -14,6 +13,21 @@ interface Props {
 
 const Tweetbox = ({ session }: Props) => {
   const [input, setInput] = useState<string>("");
+  const [image, setImage] = useState<string>("");
+  const [imageUrlBoxOpen, setImageUrlBoxOpen] = useState<boolean>(false);
+  const imageInputRef = useRef<HTMLInputElement>(null);
+
+  const addImageToTweet = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    e.preventDefault();
+
+    if (!imageInputRef.current?.value) return;
+
+    setImage(imageInputRef.current?.value);
+    imageInputRef.current.value = "";
+    setImageUrlBoxOpen(false);
+  };
 
   const iconStyles =
     "w-5 h-5 cursor-pointer transition-transform duration-150 ease-out hover:scale-150";
@@ -37,7 +51,10 @@ const Tweetbox = ({ session }: Props) => {
           />
           <div className="flex items-center">
             <div className="flex flex-1 space-x-2 text-twitter">
-              <PhotographIcon className={iconStyles} />
+              <PhotographIcon
+                onClick={() => setImageUrlBoxOpen(!imageUrlBoxOpen)}
+                className={iconStyles}
+              />
               <SearchCircleIcon className={iconStyles} />
               <EmojiHappyIcon className={iconStyles} />
               <CalendarIcon className={iconStyles} />
@@ -51,6 +68,32 @@ const Tweetbox = ({ session }: Props) => {
               Tweet
             </button>
           </div>
+
+          {imageUrlBoxOpen && (
+            <form className="flex px-4 py-2 mt-5 rounded-lg bg-twitter/80">
+              <input
+                ref={imageInputRef}
+                className="flex-1 p-2 text-white bg-transparent outline-none placeholder:text-white"
+                type="text"
+                placeholder="Enter image URL..."
+              />
+              <button
+                type="submit"
+                onClick={addImageToTweet}
+                className="font-bold text-white"
+              >
+                Add image
+              </button>
+            </form>
+          )}
+
+          {image && (
+            <img
+              className="object-contain w-full h-40 mt-10 shadow-lg rounded-xl"
+              src={image}
+              alt=""
+            />
+          )}
         </form>
       </div>
     </div>
